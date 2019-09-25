17d93d545a45ca788f6b944f0e34d5143069a4af
Juan Carlos Garcia Medina <carlos_isc7@outlook.com>
2019-09-25 11:47:21 -0500

    view para ProyectoLista

--- supertt/proyectos/views.py
+++ supertt/proyectos/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+#from django.shortcuts import render
+from rest_framework import generics
+from .models import Proyecto
+from .serializers import SerializadorProyecto
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+class ProyectoLista(generics.ListCreateAPIView):
+    queryset = Proyecto.objects.all()
+    serializer_class = SerializadorProyecto
+
+    def get_object(self):
+        queryset = self.get_queryset()
+            
+        obj = get_object_or_404(
+            queryset,
+            pk = 1,
+        )
+        return obj
