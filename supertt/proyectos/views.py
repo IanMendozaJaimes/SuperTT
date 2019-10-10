ccb85f1ee5dabb52b3d5aeef5a18d6e7458a0422
Ian Mendoza Jaimes <ianmendozajaimes@gmail.com>
2019-10-10 13:19:13 -0500

    new user

--- supertt/proyectos/views.py
+++ supertt/proyectos/views.py
@@ -4,11 +4,17 @@ from .models import Proyecto
 from .serializers import SerializadorProyecto
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
 
-class ProyectsView(TemplateView):
-	template_name = 'proyectos.html'
+class ProyectsView(LoginRequiredMixin, TemplateView):
+    login_url = '/usuarios/login'
+    template_name = 'proyectos.html'
 
 
 class TranslationsView(TemplateView):
