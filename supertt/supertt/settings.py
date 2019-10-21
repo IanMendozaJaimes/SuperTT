ea6d96e6bac5cce9db25c6d5783b71d44d458e4f
Ian Mendoza Jaimes <ianmendozajaimes@gmail.com>
2019-10-21 17:54:13 -0500

    web

--- supertt/supertt/settings.py
+++ supertt/supertt/settings.py
@@ -103,27 +103,28 @@ WSGI_APPLICATION = 'supertt.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'tt_database',
+#         'USER': 'postgres',
+#         'PASSWORD': 'newcastle',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'tt_database',
-        'USER': 'postgres',
-        'PASSWORD': 'newcastle',
+        'USER': 'ianMJ',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'tt_database',
-#         'USER': 'ianMJ',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
 # AUTHENTICATION_BACKENDS = (
 #     'django.contrib.auth.backends.ModelBackend',
 # )
