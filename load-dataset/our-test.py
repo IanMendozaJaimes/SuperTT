cd5f77e0f8380e2933e79eb67e5da220fb38ec80
Tona <carlostonatihu@gmail.com>
2020-03-27 20:19:10 -0600

    Correcciones en el conjunto de entrenamiento

--- load-dataset/our-test.py
+++ load-dataset/our-test.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
-file_path = './training.csv'
-BATCH_SIZE = 32
-IMG_HEIGHT = 155
-IMG_WIDTH = 515
+
+training_path = './CROHME_dataset_v1'
+training_file_name = 'tokenized.csv'
+training_file_path = training_path + '/' + training_file_name
+BATCH_SIZE = 100
+IMG_HEIGHT = 300
+IMG_WIDTH = 300
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 def show_batch(dataset):
@@ -12,20 +15,25 @@ def show_batch(dataset):
 			print("{:20s}: {}".format(key, value.numpy()))
 		print()
 
-dataset = tf.data.experimental.make_csv_dataset(
-      file_path,
-      batch_size=1,
-      na_value="?",
-      num_epochs=1,
-      label_name='expresion',
-      ignore_errors=True)
-
 def process_path(features, label):
 	prueba = features['imagen']
+	prueba = training_path + '/' + prueba
 	img = tf.io.read_file(prueba[0])
 	img = tf.image.decode_jpeg(img, channels=1)
 	img = tf.image.convert_image_dtype(img, tf.float32)
-	img = tf.image.resize(img, [IMG_WIDTH, IMG_HEIGHT])
 	return img, label
 
+dataset = tf.data.experimental.make_csv_dataset(
+      training_file_path,
+      batch_size=1,
+      na_value="?",
+      num_epochs=1,
+	  column_names=["imagen", "token"],
+	  label_name="token",
+      ignore_errors=True)
+
+
+# Conjunto listo
 dataset = dataset.map(process_path, num_parallel_calls=AUTOTUNE)
+for i in (dataset.take(1)):
+	print(i)
