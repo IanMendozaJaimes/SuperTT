cdee2053d8ef40fc992c4722a9518dac89e5e266
Juan Carlos Garcia Medina <carlos_isc7@outlook.com>
2020-04-16 21:33:24 -0500

    fixed comma index

--- MexpTokenizer/NSequenceToLatex.py
+++ MexpTokenizer/NSequenceToLatex.py
@@ -13,8 +13,13 @@ class Converter:
         lat = ""
         for s in seq:
             if not (s == 1000 or s == 1001):
-                lat += self.mapper[tokens[s-1]]
+                if s == 40: #if s == comma
+                    lat += ","
+                else:
+                    lat += self.mapper[tokens[ s-1 ]]
+
         return lat
 if __name__ == '__main__':
+    print(tokens.index('comma'))
     c = Converter()
-    print(c.seq2Lat([64, 33, 41, 74, 47, 58, 42, 65, 34]))
+    print(c.seq2Lat([51, 51, 61, 61, 51, 58, 46, 12, 40, 11, 40, 51, 41, 40, 51, 51, 40, 12, 40, 53, 95, 41, 33, 41]))
