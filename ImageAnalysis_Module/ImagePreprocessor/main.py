4f5dc73fae44113c2ed2f1e8b68ff9db4e18896e
Juan Carlos Garcia Medina <carlos_isc7@outlook.com>
2020-05-21 23:12:22 -0500

    process set of images

--- ImageAnalysis_Module/ImagePreprocessor/main.py
+++ ImageAnalysis_Module/ImagePreprocessor/main.py
@@ -1,8 +1,19 @@
+import os
 from scriptCV import ImageAlgorithm, ImageProcessor
 
+INP_FOLDER = "/home/qapolo/Escritorio/SuperTT/ImageAnalysis_Module/inkml2img/converted_expressions"
+OUT_FOLDER = "/home/qapolo/Escritorio/CHROME_DATASET_sauvola"
+def processFolder():
+	images =  os.listdir(INP_FOLDER)
+	for im in images:
+		print(f"{im}")
+		ip = ImageProcessor(f"{INP_FOLDER}/{im}")
+		ip.processBinarization()
+		ip.saveImage(f"{OUT_FOLDER}/{im}")
+		
 if __name__ == '__main__':
-	
-	ip = ImageProcessor("path_image")
-	ip.processBinarization()
-	ip.saveImage("path_to_save")
+	processFolder()
+	#ip = ImageProcessor("/home/qapolo/Escritorio/0051a61b-8d42-4e39-85ed-f9170d03b973.jpeg")
+	#ip.processBinarization()
+	#ip.saveImage("/home/qapolo/Escritorio/output.jpeg")
 	
--- main_db.py
+++ main_db.py
@@ -5,7 +5,7 @@ import sys
 from accesodb.base import Session, engine, Base
 from accesodb.traduccion import Traduccion
 
-from ImageAnalysis_Module.ImagePreprocessor.scriptCV import ImageProcessor
+from ImageAnalysis_Module.ImagePreprocessor.scriptCV import ImageProcessor, ImageAlgorithm
 from nn.test_model import img2latSeqConverter, initModels
 from MexpTokenizer.NSequenceToLatex import Converter
 
@@ -23,11 +23,11 @@ def procesar_traducciones(encoder, decoder, converter):
         #assert
         if os.path.exists(BASE_DIR + "/" + name_image):
             ip = ImageProcessor(BASE_DIR + "/" + name_image)
-            ip.processBinarization()
+            ip.processBinarization(algorithm=ImageAlgorithm.OTSU)
             name_image = name_image.replace("/", "_").replace(EXTENSION, ".png")
 
             if not os.path.exists(f"{BASE_DIR}/processed_images/{name_image}" ):
-                ip.saveImage(f"{BASE_DIR}/processed_images/{name_image}")
+                ip.saveImage(f"{BASE_DIR}/processed_images/{name_image}", algorithm=ImageAlgorithm.OTSU)
                 seq = img2latSeqConverter(f"{BASE_DIR}/processed_images/{name_image}", encoder, decoder)
                 
                 elemento.traduccion = converter.seq2Lat(seq)
