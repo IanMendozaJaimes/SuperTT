2ee1674230a30acf6fb815c81ea0e25cfacf64ce
Juan Carlos Garcia Medina <carlos_isc7@outlook.com>
2020-06-09 10:18:18 -0500

    sauvola algorithm

--- main_db.py
+++ main_db.py
@@ -6,8 +6,6 @@ from accesodb.base import Session, engine, Base
 from accesodb.traduccion import Traduccion
 
 from ImageAnalysis_Module.ImagePreprocessor.scriptCV import ImageProcessor, ImageAlgorithm
-#from nn.test_model import img2latSeqConverter, initModels
-#from MexpTokenizer.NSequenceToLatex import Converter
 from nn.ProductionModel.imageloader import ImagePredict
 
 BASE_DIR = "supertt/media/proyectos"
@@ -24,12 +22,11 @@ def procesar_traducciones(pred):
         #assert
         if os.path.exists(BASE_DIR + "/" + name_image):
             ip = ImageProcessor(BASE_DIR + "/" + name_image)
-            ip.processBinarization(algorithm=ImageAlgorithm.OTSU)
+            ip.processBinarization(algorithm=ImageAlgorithm.SAUVOLA)
             name_image = name_image.replace("/", "_").replace(EXTENSION, ".png")
 
             if not os.path.exists(f"{BASE_DIR}/processed_images/{name_image}" ):
-                ip.saveImage(f"{BASE_DIR}/processed_images/{name_image}", algorithm=ImageAlgorithm.OTSU)
-                #seq = img2latSeqConverter(f"{BASE_DIR}/processed_images/{name_image}", encoder, decoder)
+                ip.saveImage(f"{BASE_DIR}/processed_images/{name_image}", algorithm=ImageAlgorithm.SAUVOLA)
                 pred.load_image(f"{BASE_DIR}/processed_images/{name_image}")
                 seq = pred.predict()
                 
@@ -42,7 +39,7 @@ def procesar_traducciones(pred):
 if __name__ == "__main__":
     if not os.path.exists(f"{BASE_DIR}/processed_images"):
 	    os.mkdir(f"{BASE_DIR}/processed_images")
-    #encoder, decoder = initModels("nn")
+
     pred = ImagePredict()
     while True:
         procesar_traducciones(pred)
